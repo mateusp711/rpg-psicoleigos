e071983f66da6d43df33aee1399519bbc9e36a82
Mateus Florindo <mateus.pereira@aoatechpartners.com.br>
2023-03-26 07:20:50 -0300

    fix: #boxes layout and bubbly button

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss', './bubly-button.scss']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+
+  animateButton = (e: any) => {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+
+    e.target.classList.add('animate');
+    setTimeout(function () {
+      e.target.classList.remove('animate');
+    }, 700);
+  };
+
+  bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+
+  click() {
+    for (var i = 0; i < this.bubblyButtons.length; i++) {
+      this.bubblyButtons[i].addEventListener('click', this.animateButton, false);
+    }
+  }
 
 }
